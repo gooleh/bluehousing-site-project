1e28a131592a096dacc9e980107dd1e88fa8354c
gooleh <landrover1221@gmail.com>
2026-06-04 03:16:37 +0900

    Updates

--- static/js/720.c48510ab.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbluehousing_site_project=self.webpackChunkbluehousing_site_project||[]).push([[720],{6720:(t,e,a)=>{a.d(e,{ree:()=>i});var h=a(3441);function i(t){return(0,h.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"},child:[]},{tag:"path",attr:{d:"M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"},child:[]}]})(t)}}}]);
-//# sourceMappingURL=720.c48510ab.chunk.js.map
